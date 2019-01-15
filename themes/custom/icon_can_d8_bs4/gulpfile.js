644587f610d06f6332503b2ec76d22d2105d1ae8
Steve Gong <stevegong2016@gmail.com>
2019-01-15 16:32:46 +1100

    setup gulp tasks for gulp 4

--- themes/custom/icon_can_d8_bs4/gulpfile.js
+++ themes/custom/icon_can_d8_bs4/gulpfile.js
@@ -1,48 +1,69 @@
-var gulp = require('gulp'),
+const dist_css_base = 'css';
+const dist_js_base = 'js';
+
+let scss_files_global = [
+  'src/scss/style.scss',
+  'src/scss/maintenance-page.scss',
+  'src/scss/print.scss',
+];
+
+let scss_files_components = [
+  'src/scss/components/affix.scss',
+  'src/scss/components/book-navigation.scss',
+  'src/scss/components/breadcrumb.scss',
+  'src/scss/components/dropbutton.component.scss',
+  'src/scss/components/dropbutton.scss',
+  'src/scss/components/form.scss',
+  'src/scss/components/indented.scss',
+  'src/scss/components/node.scss',
+  'src/scss/components/sidebar-collapse.scss',
+  'src/scss/components/tabs.scss',
+  'src/scss/components/vertical-tabs.scss'
+];
+
+let scss_files_colors = [
+  'src/scss/colors/messages/messages-dark.scss',
+  'src/scss/colors/messages/messages-light.scss'
+];
+
+
+let js_files_for_compiling = 'src/js/*.js';
+
+// Error Handling to stop file watching from dying on an error (ie: Sass
+// compiling).
+let onError = function (err) {
+  beeper();
+  console.log(err);
+};
+
+const gulp = require('gulp'),
     sass = require('gulp-sass'),
     prefix = require('gulp-autoprefixer'),
     clean = require('gulp-clean'),
     cleanCSS = require('gulp-clean-css'),
     uglify = require('gulp-uglify'),
     plumber = require('gulp-plumber'), // For error handling.
-    gutil = require('gulp-util'); // For error handling.
-
-const scss_files_for_compiling = [
-  'scss/style.scss',
-  'scss/components/affix.scss',
-  'scss/components/book-navigation.scss',
-  'scss/components/breadcrumb.scss',
-  'scss/components/dropbutton.component.scss',
-  'scss/components/dropbutton.scss',
-  'scss/components/form.scss',
-  'scss/components/indented.scss',
-  'scss/components/maintenance-page.scss',
-  'scss/components/node.scss',
-  'scss/components/sidebar-collapse.scss',
-  'scss/components/tabs.scss',
-  'scss/components/vertical-tabs.scss',
-  'scss/colors/messages/messages-dark.scss',
-  'scss/colors/messages/messages-light.scss'
-];
-
-// Error Handling to stop file watching from dying on an error (ie: Sass
-// compiling).
-var onError = function (err) {
-  gutil.beep();
-  console.log(err);
-};
+    beeper = require('beeper'); // For error handling.
 
 /**
- * @task sass
- * Compile files from scss
+ * @task styles
+ * Compile files from scss files
  */
-gulp.task('styles', function () {
+function styles() {
+  // Compile global styles
+  compile_scss_files(scss_files_global, dist_css_base);
+  // Compile components
+  compile_scss_files(scss_files_components, dist_css_base + '/components/');
+  // Compile colors
+  return compile_scss_files(scss_files_colors, dist_css_base + '/colors/messages/');
+}
 
+function compile_scss_files(src, dist){
   /**
    * Match bootstrap upstream requirements.
    * @type Array
    */
-  var prefixOptions = [
+  const prefixOptions = [
     "Android >= 5",
     "Chrome >= 49",
     "Firefox >= 35",
@@ -52,60 +73,71 @@ gulp.task('styles', function () {
     "Safari >= 10"
   ];
 
-  gulp.src('css/*', {read: false}).pipe(clean());
+  return gulp.src(src) // the source .scss file
+      .pipe(sass().on('error', sass.logError))
+      .pipe(prefix({browsers: prefixOptions, cascade: true})) // pass the file
+      // through
+      // autoprefixer
+      .pipe(cleanCSS())
+      .pipe(gulp.dest(dist));
+}
 
+function clean_part() {
+  return gulp.src([
+        dist_css_base + '/colors/*',
+        dist_css_base + '/components/*',
+        dist_css_base + '/*.css',
+        dist_js_base + '/*.js',],
+      {read: false}).pipe(clean());
+}
+
+function clean_all() {
+  return gulp.src([dist_css_base + '/*', dist_js_base + '/*',], {read: false}).pipe(clean());
+}
+
+function move_min_css_n_js(){
   // Min CSS file Movement
   gulp.src('node_modules/jquery.mmenu/dist/jquery.mmenu.all.css')
       .pipe(plumber({
         errorHandler: onError
       }))
-      .pipe(gulp.dest('css'));
-
-  return gulp.src(scss_files_for_compiling) // the source .scss file
-      .pipe(sass().on('error', sass.logError))
-      .pipe(prefix({browsers: prefixOptions, cascade: true})) // pass the file through autoprefixer
-      .pipe(cleanCSS())
-      .pipe(gulp.dest('css'))
-});
-
-/**
- * @task scripts
- * Compile files from scripts
- */
-gulp.task('scripts', function () {
+      .pipe(gulp.dest(dist_css_base + '/vendors'));
 
   // Min JS files movement
-  gulp.src([
-    'node_modules/bootstrap/dist/js/**',
+  return gulp.src([
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/popper.js/dist/umd/popper.min.js',
     'node_modules/jquery.mmenu/dist/jquery.mmenu.all.js'
   ])
       .pipe(plumber({
         errorHandler: onError
       }))
-      .pipe(gulp.dest('js'));
+      .pipe(gulp.dest(dist_js_base + '/vendors'));
+}
 
-  return gulp.src('src/js/*.js')
+/**
+ * @task scripts
+ * Compile files from scripts
+ */
+function scripts() {
+  return gulp.src(js_files_for_compiling)
       .pipe(plumber({
         errorHandler: onError
       }))
       .pipe(uglify())
-      .pipe(gulp.dest('js'));
+      .pipe(gulp.dest(dist_js_base));
+}
 
-});
+
+// Default tasks
+exports.default = gulp.series(clean_all, move_min_css_n_js, styles, scripts);
 
 
-//Watch task
-gulp.task('default', ['styles', 'scripts']);
+// Watch tasks
+let total_watching_files = scss_files_global.concat(scss_files_components, scss_files_colors, js_files_for_compiling);
 
-// Watch changes.
-gulp.task('watch', ['scripts', 'styles'], function() {
-  // Watch for JS changes.
-  gulp.watch('src/js/*.js', function() {
-    gulp.start('scripts');
-  });
-  // Watch for Sass changes.
-  gulp.watch(scss_files_for_compiling, function() {
-    gulp.start('styles');
-  });
+gulp.watch(total_watching_files, function () {
+  clean_part();
+  styles();
+  scripts()
 });
