4bddf7aa5f29e1b9bbfeaf6c57362324f264266a
Steve Gong <stevegong2016@gmail.com>
2019-01-16 12:53:41 +1100

    use gulp 3 instead of 4; revised gulpfile.js and package.json to achieve stable compiling outcome; adjusted library file to accomodate new dist css structure, still need more work to do; recompiled all FE files.

--- themes/custom/icon_can_d8_bs4/gulpfile.js
+++ themes/custom/icon_can_d8_bs4/gulpfile.js
@@ -1,64 +1,43 @@
-const dist_css_base = 'css';
-const dist_js_base = 'js';
+const dist_css_base = './css';
+const dist_js_base = './js';
 
-let scss_files_global = [
-  'src/scss/style.scss',
-  'src/scss/maintenance-page.scss',
-  'src/scss/print.scss',
+const src_move_css_files = ['./node_modules/jquery.mmenu/dist/jquery.mmenu.all.css'];
+
+const src_move_js_files = [
+  './node_modules/bootstrap/dist/js/bootstrap.min.js',
+  './node_modules/popper.js/dist/umd/popper.min.js',
+  './node_modules/jquery.mmenu/dist/jquery.mmenu.all.js'
 ];
 
-let scss_files_components = [
-  'src/scss/components/affix.scss',
-  'src/scss/components/book-navigation.scss',
-  'src/scss/components/breadcrumb.scss',
-  'src/scss/components/dropbutton.component.scss',
-  'src/scss/components/dropbutton.scss',
-  'src/scss/components/form.scss',
-  'src/scss/components/indented.scss',
-  'src/scss/components/node.scss',
-  'src/scss/components/sidebar-collapse.scss',
-  'src/scss/components/tabs.scss',
-  'src/scss/components/vertical-tabs.scss'
-];
-
-let scss_files_colors = [
-  'src/scss/colors/messages/messages-dark.scss',
-  'src/scss/colors/messages/messages-light.scss'
-];
-
-
-let js_files_for_compiling = 'src/js/*.js';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const plumber = require('gulp-plumber'); // For error handling.
+const gutil = require('gulp-util'); // For error handling.
 
 // Error Handling to stop file watching from dying on an error (ie: Sass
 // compiling).
 let onError = function (err) {
-  beeper();
+  gutil.beep();
   console.log(err);
 };
 
-const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    prefix = require('gulp-autoprefixer'),
-    clean = require('gulp-clean'),
-    cleanCSS = require('gulp-clean-css'),
-    uglify = require('gulp-uglify'),
-    plumber = require('gulp-plumber'), // For error handling.
-    beeper = require('beeper'); // For error handling.
-
+// ******** CSS related functions and tasks ********
 /**
- * @task styles
- * Compile files from scss files
+ * @task Default sass task
+ * Compile files from scss
  */
-function styles() {
-  // Compile global styles
-  compile_scss_files(scss_files_global, dist_css_base);
-  // Compile components
-  compile_scss_files(scss_files_components, dist_css_base + '/components/');
-  // Compile colors
-  return compile_scss_files(scss_files_colors, dist_css_base + '/colors/messages/');
-}
+gulp.task('styles', function () {
+  gulp.src(dist_css_base + '/*', {read: false}).pipe(clean());
 
-function compile_scss_files(src, dist){
+  // Min CSS file Movement
+  gulp.src(src_move_css_files)
+      .pipe(plumber({errorHandler: onError}))
+      .pipe(gulp.dest(dist_css_base + '/'));
+
   /**
    * Match bootstrap upstream requirements.
    * @type Array
@@ -73,71 +52,54 @@ function compile_scss_files(src, dist){
     "Safari >= 10"
   ];
 
-  return gulp.src(src) // the source .scss file
+  // Compile styles
+  return gulp.src('./src/scss/*.scss') // the source .scss file
       .pipe(sass().on('error', sass.logError))
-      .pipe(prefix({browsers: prefixOptions, cascade: true})) // pass the file
-      // through
-      // autoprefixer
+      .pipe(prefix({browsers: prefixOptions, cascade: true})) // pass the file through autoprefixer
       .pipe(cleanCSS())
-      .pipe(gulp.dest(dist));
-}
+      .pipe(gulp.dest(dist_css_base + '/'));
+});
 
-function clean_part() {
-  return gulp.src([
-        dist_css_base + '/colors/*',
-        dist_css_base + '/components/*',
-        dist_css_base + '/*.css',
-        dist_js_base + '/*.js',],
-      {read: false}).pipe(clean());
-}
-
-function clean_all() {
-  return gulp.src([dist_css_base + '/*', dist_js_base + '/*',], {read: false}).pipe(clean());
-}
-
-function move_min_css_n_js(){
-  // Min CSS file Movement
-  gulp.src('node_modules/jquery.mmenu/dist/jquery.mmenu.all.css')
-      .pipe(plumber({
-        errorHandler: onError
-      }))
-      .pipe(gulp.dest(dist_css_base + '/vendors'));
-
-  // Min JS files movement
-  return gulp.src([
-    'node_modules/bootstrap/dist/js/bootstrap.min.js',
-    'node_modules/popper.js/dist/umd/popper.min.js',
-    'node_modules/jquery.mmenu/dist/jquery.mmenu.all.js'
-  ])
-      .pipe(plumber({
-        errorHandler: onError
-      }))
-      .pipe(gulp.dest(dist_js_base + '/vendors'));
-}
-
+// ******** JS related functions and tasks ********
 /**
  * @task scripts
  * Compile files from scripts
  */
-function scripts() {
-  return gulp.src(js_files_for_compiling)
+gulp.task('scripts', function () {
+  gulp.src(dist_js_base + '/*', {read: false}).pipe(clean());
+
+  // Min JS files movement
+  gulp.src(src_move_js_files)
       .pipe(plumber({
         errorHandler: onError
       }))
+      .pipe(gulp.dest(dist_js_base));
+
+  return gulp.src('./src/js/**/*.js')
+      .pipe(plumber({errorHandler: onError}))
       .pipe(uglify())
       .pipe(gulp.dest(dist_js_base));
-}
+});
 
+// ********************************************************************************************************************************************
 
-// Default tasks
-exports.default = gulp.series(clean_all, move_min_css_n_js, styles, scripts);
+// Default gulp task.
+gulp.task('default', function () {
+  gulp.start('styles');
+  gulp.start('scripts');
+});
 
+// Watch changes.
+gulp.task('watch', function () {
 
-// Watch tasks
-let total_watching_files = scss_files_global.concat(scss_files_components, scss_files_colors, js_files_for_compiling);
+  // Watch for Sass changes.
+  gulp.watch('./src/scss/**/*.scss', function () {
+    gulp.start('styles');
+  });
 
-gulp.watch(total_watching_files, function () {
-  clean_part();
-  styles();
-  scripts()
+  // Watch for JS changes.
+  gulp.watch('./src/js/**/*.js', function () {
+    gulp.start('scripts');
+  });
+
 });
